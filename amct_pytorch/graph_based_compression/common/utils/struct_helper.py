e568df75a09ed5d154e7f30f31bb504f64faea1a
fujun19 <fujun19@hisilicon.com>
2026-04-23 16:22:06 +0800

    自动混合精度特性下线
    Co-authored-by: fujun19<fujun19@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !81 merge delete_feature into master
    
    自动混合精度特性下线
    
    Created-by: fujun19
    Commit-by: fujun19
    Merged-by: cann-robot
    Description: ## 描述
    自动混合精度特性下线
    
    ## 如何测试
    bash build.sh -u执行全量用例，用例执行成功不报错
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 代码重构
    - [x] 其他，请描述：特性下线
    
    
    
    
    See merge request: cann/amct!81

--- amct_pytorch/graph_based_compression/common/utils/struct_helper.py
+++ amct_pytorch/graph_based_compression/common/utils/struct_helper.py
@@ -25,7 +25,6 @@ from ..auto_calibration.sensitivity_base import SensitivityBase
 from ..auto_calibration.sensitivity_base import MseSensitivity
 from . import files as files_util
 from . import vars_util
-from ..config.auto_mix_quant_config import AutoMixedPrecisionConfigHelper
 
 
 class DumpConfig:
@@ -73,72 +72,3 @@ class CalibrationConfigInfo:
         if self.config_defination is not None and not os.path.exists(self.config_defination):
             raise RuntimeError("The config_defination {} does not exist, please check the file path.".format(
                 self.config_defination))
-
-
-class AutoSearchMixedPrecisionInfo:
-    """ Config info for AutoSearchMixedPrecision. """
-    def __init__(self, cfg_file, save_dir, sensitivity):
-        """ Init func.
-
-        Args:
-            cfg_file (string): file from AutoMixedPrecisionConfig, indicating how to do search.
-            save_dir (string): the path where to store model and model's name.
-        """
-        # set variable by config
-        self.cfg_helper = AutoMixedPrecisionConfigHelper(AutoMixedPrecisionConfigHelper.read_file(cfg_file))
-        self.save_dir, self.save_prefix = files_util.split_save_path(save_dir)
-        self.sensitivity = sensitivity
-        self.check_params()
-
-        # set inner variable
-        self.quant_mix_bits = [vars_util.FP16_BIT, vars_util.INT8_BIT, vars_util.INT4_BIT]
-        self.acc_decay = {}
-        self.computes_ops = {}
-        self.computes_ops_constraint = 0
-        self.bit_config = {}
-        self.quant_layers = []
-        self.shape_info = {}
-        self.qat_search_range = {}
-        self.qat_precision_file = files_util.concat_name(self.save_dir, self.save_prefix, 'qat_mixed_precision.json')
-        self.qat_cfg_file = files_util.concat_name(self.save_dir, self.save_prefix, 'qat_mixed_precision.cfg')
-
-        files_util.create_file_path(self.save_dir)
-
-    def check_params(self):
-        """ check params """
-        self.cfg_helper.parse()
-        self.compression_ratio = self.cfg_helper.compress_ratio
-
-        if isinstance(self.sensitivity, SensitivityBase):
-            pass
-        elif self.sensitivity != 'MseSimilarity':
-            raise ValueError("{} is not support. Only support ['MseSimilarity']."
-                    .format(self.sensitivity))
-        else:
-            self.sensitivity = MseSensitivity(do_normalization=True)
-
-
-class AutoSearchMixedPrecisionTempInfo:
-    """ Config info for AutoSearchMixedPrecision tmp using. """
-    def __init__(self, temp_dir):
-        """ Init func.
-
-        Args:
-            temp_dir (string): path of temp folder.
-        """
-        self.temp_dir = temp_dir
-
-        self.dump_dir = os.path.join(self.temp_dir, "dump")
-        files_util.create_path(self.dump_dir)
-
-        self.config_file = os.path.join(self.temp_dir, "calibration_config.json")
-        self.int8_config_file = os.path.join(self.temp_dir, "int8_calibration_config.json")
-        self.int8_record_file = os.path.join(self.temp_dir, "int8_record.txt")
-        self.int4_config_file = os.path.join(self.temp_dir, "int4_calibration_config.json")
-        self.int4_record_file = os.path.join(self.temp_dir, "int4_record.txt")
-        self.loss_info_file = os.path.join(self.temp_dir, "loss_info.txt")
-        self.bitops_info_file = os.path.join(self.temp_dir, "bitops_info.txt")
-        self.shape_info_file = os.path.join(self.temp_dir, "shape_info.txt")
-
-        self.fused_file = os.path.join(self.temp_dir, "fused.pb")
-        self.modified_onnx_file = os.path.join(self.temp_dir, 'modified_model.onnx')
